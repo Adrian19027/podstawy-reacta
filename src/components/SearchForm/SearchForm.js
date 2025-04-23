290cc9f19b4d47c776419d04e6de8b5da35aa394
Adrian Kotowski <adekkotowski1@wp.pl>
2025-04-23 14:12:41 +0200

    Add functionality to SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,27 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+
+    const dispatch = useDispatch();
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: inputValue});
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form onSubmit={handleSubmit} className={styles.searchForm}>
+            <TextInput
+                placeholder="Search..."
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+            />
             <Button>
                 <span className="fa fa-search" />
             </Button>
