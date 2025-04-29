bb5d8dd15fc299d8bfd00bebaa1cdf854031a963
Adrian Kotowski <adekkotowski1@wp.pl>
2025-04-29 20:25:50 +0200

    Add functionality of routing and few subpages

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import store from './redux/store';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
