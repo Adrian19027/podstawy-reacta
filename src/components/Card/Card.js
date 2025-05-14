f66a11c3cbf3ef136feb906d012ba810e1f53cc5
Adrian Kotowski <adekkotowski1@wp.pl>
2025-05-14 13:24:35 +0200

    Add action REMOVE_BOOK

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,6 @@
 import { useDispatch} from 'react-redux';
 import styles from './Card.module.scss';
-import { toggleFavorite } from '../../redux/cardsRedux.js';
+import { toggleFavorite, removeCard } from '../../redux/cardsRedux.js';
 
 const Card = ({id, title, isFavorite}) => {
 
@@ -10,11 +10,19 @@ const Card = ({id, title, isFavorite}) => {
     const handleToggleFavorite = () => {
         dispatch(toggleFavorite(id));
     }
+
+    const handleRemoveCard = () => {
+        dispatch(removeCard(id))
+    }
     return (
-        <li className={styles.card}>{title}
-            <button className={`fa ${isFavorite ? 'fa-star' : 'fa-star-o'} ${styles.favoriteButton}`}
-            onClick={handleToggleFavorite}
-            ></button>
+        <li className={styles.card}>
+            <div className={styles.cardContent}>{title}</div>
+            <div className={styles.cardActions}>
+                <button type="button" className={`fa ${isFavorite ? 'fa-star' : 'fa-star-o'} ${styles.favoriteButton}`}
+                onClick={handleToggleFavorite}
+                ></button>
+                <button type="button" className={`fa fa-trash ${styles.deleteButton}`} onClick={handleRemoveCard}></button>
+            </div>
         </li>
     );
 };
